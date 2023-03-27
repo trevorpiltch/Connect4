8a61565959447c63f17bf3f136b5b9b0d8249967
Trevor Piltch <trevor@piltch.com>
2023-03-27 13:44:17 -0400

    Initial Commit

---
+++ ConnectFour/ConnectFourApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectFourApp.swift
+//  ConnectFour
+//
+//  Created by Trevor Piltch on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ConnectFourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
